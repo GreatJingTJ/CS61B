da08b7a66b0bd6132da93a45e6986d94bf7f3b48
GreatJingTJ <43863396+GreatJingTJ@users.noreply.github.com>
2020-11-21 22:52:00 -0800

    Update LinkedListDeque.java

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -19,7 +19,7 @@ public class LinkedListDeque<T> {
         }
     }
 
-    public static void main(String[] args){
+    private static void main(String[] args){
         LinkedListDeque<Integer> mylist = new LinkedListDeque<>();
         System.out.print(mylist.isEmpty());
         mylist.addFirst(1);
