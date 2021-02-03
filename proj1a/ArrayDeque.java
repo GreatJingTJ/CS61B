bb13588fc4b91ec339db4a347b250cd11ed92861
GreatJingTJ <43863396+GreatJingTJ@users.noreply.github.com>
2021-02-02 22:26:06 -0800

    Update ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,45 +1,68 @@
 public class ArrayDeque<T> {
     private T[] array;
     private int size;
-    private int start_pos, end_pos;
+    private final int firstPos = 0;
+    private final int endPos = 8;
 
-    private void resize(int capacity) {
-        T[] a = (T[]) new Object[capacity];
-        System.arraycopy(this.array, 0, a, 0, this.array.length);
-        this.array = a;
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        size = 0;
+    }
+
+    public ArrayDeque(ArrayDeque other) {
+
+        this.array = (T[]) new Object[other.size()];
+        this.size = 0;
+
+        for (int i = 0; i < other.size(); i++) {
+            this.addLast((T) other.get(i));
+        }
+    }
+
+    private static void main(String[] args) {
+        ArrayDeque<Integer> myarray = new ArrayDeque<Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            myarray.addFirst(i);
+        }
+        myarray.printList();
+    }
+
+    private void resize(int n) {
+        T[] newarray = (T[]) new Object[n];
+        System.arraycopy(array, 0, newarray, 0, array.length);
+        this.array = newarray;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 
     public void addFirst(T item) {
-        if (this.size == array.length) {
+        if (this.size == this.array.length) {
             this.resize(this.size + 1);
         }
-        if(this.start_pos - 1 == -1){
-            this.start_pos = this.array.length;
-        }
-        this.start_pos -= 1;
-        this.array[this.start_pos] = item;
 
-        this.size += 1;
+        if (this.size == 0) {
+            this.array[0] = item;
+            this.size += 1;
+        } else {
+            System.arraycopy(array, 0, array, 1, array.length - 1);
+            array[0] = item;
+            this.size += 1;
+        }
     }
 
     public void addLast(T item) {
         if (this.size == this.array.length) {
             this.resize(this.size + 1);
         }
-        if(this.end_pos + 1 == this.array.length){
-            this.end_pos = 0;
-        }
-        this.array[this.end_pos] = item;
-        this.end_pos += 1;
+        this.array[this.size] = item;
         this.size += 1;
-    }
-
-    public boolean isEmpty() {
-        return (this.size == 0);
-    }
-
-    public int size() {
-        return this.size;
     }
 
     public void printDeque() {
@@ -51,91 +74,35 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
-        if (this.size == 0) {
+        if (this.size() == 0) {
             return null;
         }
-        if(this.start_pos == this.array.length){
-            this.start_pos = 0;
-        }
-
-        T ans = this.array[this.start_pos];
-        if(this.start_pos + 1 <= this.array.length){
-            this.start_pos += 1;
-        }
-
+        T itemVal = this.array[0];
+        System.arraycopy(array, 1, array, 0, this.size - 1);
         this.size -= 1;
-       return ans;
+        return itemVal;
     }
 
     public T removeLast() {
         if (this.size == 0) {
             return null;
         }
-        T ans;
-        if(this.end_pos == 0){
-            this.end_pos = this.array.length - 1;
-            ans = this.array[this.end_pos];
-        } else {
-            ans = this.array[this.end_pos - 1];
-        }
-        this.end_pos -= 1;
+        T itemVal = this.array[this.size - 1];
         this.size -= 1;
-        return ans;
+
+        return itemVal;
     }
 
-    private void printD() {
-        if (start_pos < end_pos ) {
-            for (int i = this.start_pos; i < this.end_pos; i++) {
-                System.out.print(this.array[i] + " ");
-            }
-        } else {
-            for (int i = this.start_pos; i < this.array.length; i++) {
-                System.out.print(this.array[i] + " ");
-            }
+    public T get(int index) {
+        return this.array[index];
+    }
 
-            for (int i = 0; i < this.end_pos; i++) {
-                System.out.print(this.array[i] + " ");
-            }
+    private void printList() {
+        for (int i = 0; i < this.size; i++) {
+            System.out.print(this.array[i] + " ");
         }
+
         System.out.print("\n");
     }
 
-
-    public T get(int index) {
-        if(start_pos < end_pos){
-            return this.array[this.start_pos + index];
-        }else{
-            return this.array[this.array.length - this.start_pos + index];
-        }
-    }
-
-    public ArrayDeque() {
-        this.array = (T[]) new Object[8];
-        this.size = 0;
-        this.start_pos = 8;
-        this.end_pos = 0;
-    }
-
-    private ArrayDeque(ArrayDeque other) {
-        this.start_pos = 8;
-        this.end_pos = 0;
-        this.array = (T[]) new Object[8];
-        this.size = 0;
-
-        for (int i = 0; i < other.size(); i++) {
-            this.addLast((T) other.get(i));
-        }
-    }
-
-    private static void main(String[] args) {
-        ArrayDeque<Integer> mylist = new ArrayDeque<>();
-
-        for (int i = 0; i < 5; i++) {
-            mylist.addLast(i);
-        }
-        mylist.printD();
-        ArrayDeque<Integer> mylist2 = new ArrayDeque<>(mylist);
-        mylist2.removeLast();
-        mylist2.printD();
-    }
 }
